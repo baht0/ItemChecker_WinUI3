8e84330d54be603120a293249129530e7bab408c
Rasim <bahtiarov.rasim@gmail.com>
2023-09-10 20:57:13 +0300

    service and account settings are combined

--- ItemChecker/Properties/AppProperties.cs
+++ ItemChecker/Properties/AppProperties.cs
@@ -5,17 +5,61 @@ namespace ItemChecker.Properties
 {
     internal class AppProperties
     {
-        public static UserProperties User { get; set; } = new();
+        public static AccountProperties Account { get; set; } = new();
         public static SettingsProperties Settings { get; set; } = new();
-        public static ServicesProperties Services { get; set; } = new();
         public static RareProperties Rare { get; set; } = new();
 
         public AppProperties()
         {
-            //ApplicationData.Current.LocalSettings.Values.Clear();
+            ApplicationData.Current.LocalSettings.Values.Clear();
         }
     }
 
+    public class AccountProperties
+    {
+        readonly ApplicationDataContainer LocalSettings = ApplicationData.Current.LocalSettings;
+        readonly ApplicationDataCompositeValue Composite = new();
+
+        public int SteamCurrencyId
+        {
+            get
+            {
+                if (Composite[nameof(SteamCurrencyId)] != null) //load saved
+                    steamCurrencyId = (int)Composite[nameof(SteamCurrencyId)];
+                return steamCurrencyId;
+            }
+            set
+            {
+                steamCurrencyId = value;
+                Composite[nameof(SteamCurrencyId)] = value;
+                LocalSettings.Values[nameof(AccountProperties)] = Composite;
+            }
+        }
+        int steamCurrencyId;
+
+        public bool BUFF
+        {
+            get
+            {
+                if (Composite[nameof(BUFF)] != null) //load saved
+                    buff = (bool)Composite[nameof(BUFF)];
+                return buff;
+            }
+            set
+            {
+                buff = value;
+                Composite[nameof(BUFF)] = value;
+                LocalSettings.Values[nameof(AccountProperties)] = Composite;
+            }
+        }
+        bool buff = true;
+
+        public AccountProperties()
+        {
+            Composite = (ApplicationDataCompositeValue)LocalSettings.Values[nameof(AccountProperties)];
+            Composite ??= new();
+        }
+    }
     public class RareProperties
     {
         readonly ApplicationDataContainer LocalSettings = ApplicationData.Current.LocalSettings;
@@ -26,145 +70,85 @@ namespace ItemChecker.Properties
             get
             {
                 if (Composite[nameof(MaxFloatFN)] != null) //load saved
-                    _maxFloatFN = (double)Composite[nameof(MaxFloatFN)];
-                return _maxFloatFN;
+                    maxFloatFN = (double)Composite[nameof(MaxFloatFN)];
+                return maxFloatFN;
             }
             set
             {
-                _maxFloatFN = value;
+                maxFloatFN = value;
                 Composite[nameof(MaxFloatFN)] = value;
                 LocalSettings.Values[nameof(RareProperties)] = Composite;
             }
         }
-        double _maxFloatFN = 0.001d;
+        double maxFloatFN = 0.001d;
         public double MaxFloatMW
         {
             get
             {
                 if (Composite[nameof(MaxFloatMW)] != null) //load saved
-                    _maxFloatMW = (double)Composite[nameof(MaxFloatMW)];
-                return _maxFloatMW;
+                    maxFloatMW = (double)Composite[nameof(MaxFloatMW)];
+                return maxFloatMW;
             }
             set
             {
-                _maxFloatMW = value;
+                maxFloatMW = value;
                 Composite[nameof(MaxFloatMW)] = value;
                 LocalSettings.Values[nameof(RareProperties)] = Composite;
             }
         }
-        double _maxFloatMW = 0.080d;
+        double maxFloatMW = 0.080d;
         public double MaxFloatFT
         {
             get
             {
                 if (Composite[nameof(MaxFloatFT)] != null) //load saved
-                    _maxFloatFT = (double)Composite[nameof(MaxFloatFT)];
-                return _maxFloatFT;
+                    maxFloatFT = (double)Composite[nameof(MaxFloatFT)];
+                return maxFloatFT;
             }
             set
             {
-                _maxFloatFT = value;
+                maxFloatFT = value;
                 Composite[nameof(MaxFloatFT)] = value;
                 LocalSettings.Values[nameof(RareProperties)] = Composite;
             }
         }
-        double _maxFloatFT = 0.175d;
+        double maxFloatFT = 0.175d;
         public double MaxFloatWW
         {
             get
             {
                 if (Composite[nameof(MaxFloatWW)] != null) //load saved
-                    _maxFloatWW = (double)Composite[nameof(MaxFloatWW)];
-                return _maxFloatWW;
+                    maxFloatWW = (double)Composite[nameof(MaxFloatWW)];
+                return maxFloatWW;
             }
             set
             {
-                _maxFloatWW = value;
+                maxFloatWW = value;
                 Composite[nameof(MaxFloatWW)] = value;
                 LocalSettings.Values[nameof(RareProperties)] = Composite;
             }
         }
-        double _maxFloatWW = 0.400d;
+        double maxFloatWW = 0.400d;
         public double MaxFloatBS
         {
             get
             {
                 if (Composite[nameof(MaxFloatBS)] != null) //load saved
-                    _maxFloatBS = (double)Composite[nameof(MaxFloatBS)];
-                return _maxFloatBS;
+                    maxFloatBS = (double)Composite[nameof(MaxFloatBS)];
+                return maxFloatBS;
             }
             set
             {
-                _maxFloatBS = value;
+                maxFloatBS = value;
                 Composite[nameof(MaxFloatBS)] = value;
                 LocalSettings.Values[nameof(RareProperties)] = Composite;
             }
         }
-        double _maxFloatBS = 0.500d;
+        double maxFloatBS = 0.500d;
 
         public RareProperties()
         {
             Composite = (ApplicationDataCompositeValue)LocalSettings.Values[nameof(RareProperties)];
-            Composite ??= new();
-        }
-    }
-    public class ServicesProperties
-    {
-        readonly ApplicationDataContainer LocalSettings = ApplicationData.Current.LocalSettings;
-        readonly ApplicationDataCompositeValue Composite = new();
-
-        public bool CSM
-        {
-            get
-            {
-                if (Composite[nameof(CSM)] != null) //load saved
-                    _csm = (bool)Composite[nameof(CSM)];
-                return _csm;
-            }
-            set
-            {
-                _csm = value;
-                Composite[nameof(CSM)] = value;
-                LocalSettings.Values[nameof(ServicesProperties)] = Composite;
-            }
-        }
-        bool _csm = true;
-        public bool LFM
-        {
-            get
-            {
-                if (Composite[nameof(LFM)] != null) //load saved
-                    _lfm = (bool)Composite[nameof(LFM)];
-                return _lfm;
-            }
-            set
-            {
-                _lfm = value;
-                Composite[nameof(LFM)] = value;
-                LocalSettings.Values[nameof(ServicesProperties)] = Composite;
-            }
-        }
-        bool _lfm = true;
-        public bool BUFF
-        {
-            get
-            {
-                if (Composite[nameof(BUFF)] != null) //load saved
-                    _buff = (bool)Composite[nameof(BUFF)];
-                return _buff;
-            }
-            set
-            {
-                _buff = value;
-                Composite[nameof(BUFF)] = value;
-                LocalSettings.Values[nameof(ServicesProperties)] = Composite;
-            }
-        }
-        bool _buff = true;
-
-        public ServicesProperties()
-        {
-            Composite = (ApplicationDataCompositeValue)LocalSettings.Values[nameof(ServicesProperties)];
             Composite ??= new();
         }
     }
@@ -178,65 +162,37 @@ namespace ItemChecker.Properties
             get
             {
                 if (Composite[nameof(ThemeId)] != null) //load saved
-                    _themeId = (int)Composite[nameof(ThemeId)];
-                return _themeId;
+                    themeId = (int)Composite[nameof(ThemeId)];
+                return themeId;
             }
             set
             {
-                _themeId = value;
+                themeId = value;
                 Composite[nameof(ThemeId)] = value;
                 LocalSettings.Values[nameof(SettingsProperties)] = Composite;
             }
         }
-        int _themeId;
+        int themeId;
         public bool IsMicaTheme
         {
             get
             {
                 if (Composite[nameof(IsMicaTheme)] != null) //load saved
-                    _isMicaTheme = (bool)Composite[nameof(IsMicaTheme)];
-                return _isMicaTheme;
+                    isMicaTheme = (bool)Composite[nameof(IsMicaTheme)];
+                return isMicaTheme;
             }
             set
             {
-                _isMicaTheme = value;
+                isMicaTheme = value;
                 Composite[nameof(IsMicaTheme)] = value;
                 LocalSettings.Values[nameof(SettingsProperties)] = Composite;
             }
         }
-        bool _isMicaTheme = MicaController.IsSupported();
+        bool isMicaTheme = MicaController.IsSupported();
 
         public SettingsProperties()
         {
             Composite = (ApplicationDataCompositeValue)LocalSettings.Values[nameof(SettingsProperties)];
-            Composite ??= new();
-        }
-    }
-    public class UserProperties
-    {
-        readonly ApplicationDataContainer LocalSettings = ApplicationData.Current.LocalSettings;
-        readonly ApplicationDataCompositeValue Composite = new();
-
-        public int SteamCurrencyId
-        {
-            get
-            {
-                if (Composite[nameof(SteamCurrencyId)] != null) //load saved
-                    _steamCurrencyId = (int)Composite[nameof(SteamCurrencyId)];
-                return _steamCurrencyId;
-            }
-            set
-            {
-                _steamCurrencyId = value;
-                Composite[nameof(SteamCurrencyId)] = value;
-                LocalSettings.Values[nameof(UserProperties)] = Composite;
-            }
-        }
-        int _steamCurrencyId;
-
-        public UserProperties()
-        {
-            Composite = (ApplicationDataCompositeValue)LocalSettings.Values[nameof(UserProperties)];
             Composite ??= new();
         }
     }
