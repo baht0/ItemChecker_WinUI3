a7b6f4879fad083ac9256bc6c83be0752b5b254f
Rasim <bahtiarov.rasim@gmail.com>
2023-09-10 20:57:38 +0300

    added MessageEvent

--- ItemChecker/ViewModels/BaseViewModel.cs
+++ ItemChecker/ViewModels/BaseViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using ItemChecker.Models;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Threading;
@@ -11,16 +12,21 @@ namespace ItemChecker.ViewModels
         public List<T> InitialItems { get; set; } = new();
 
         [ObservableProperty]
-        ObservableCollection<T> _items = new();
+        ObservableCollection<T> items = new();
 
         [ObservableProperty]
-        MessageBar _messageBar = new();
-        [ObservableProperty]
-        Dictionary<string, object> _info = new();
+        Dictionary<string, object> info = new();
 
         public int Secounds { get; set; }
 
         public CancellationTokenSource CTSource { get; set; } = new();
         public CancellationToken CToken { get; set; }
+
+        public event EventHandler MessageEvent;
+        public void MessageShow(string title, string message, int icon = 0)
+        {
+            var args = new MessageEventArgs(title, message, icon);
+            MessageEvent?.Invoke(this, args);
+        }
     }
 }
