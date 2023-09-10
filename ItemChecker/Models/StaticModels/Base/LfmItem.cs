5c050e5f2b3822f671f082b4ab5b961245a7b8f2
Rasim <bahtiarov.rasim@gmail.com>
2023-09-10 20:50:59 +0300

    added JsonProperty

--- ItemChecker/Models/StaticModels/Base/LfmItem.cs
+++ ItemChecker/Models/StaticModels/Base/LfmItem.cs
@@ -1,18 +1,33 @@
-﻿using System;
-using ItemChecker.Models.StaticModels;
+﻿using Newtonsoft.Json;
 
 namespace ItemChecker.Models.StaticModels.Base
 {
     public class LfmItem
     {
-        public DateTime Updated { get; set; } = DateTime.Now.AddHours(-1);
-        public double Price { get; set; }
+        [JsonProperty("price")]
+        public double Price
+        {
+            get => price / 100;
+            set => price = value;
+        }
+        private double price;
+        [JsonProperty("have")]
         public int Have { get; set; }
+        [JsonProperty("max")]
         public int Limit { get; set; }
+        [JsonProperty("res")]
         public int Reservable { get; set; }
+        [JsonProperty("tr")]
         public int Tradable { get; set; }
-        public double SteamPriceRate { get; set; }
-        public bool IsHave { get; set; }
-        public ItemStatus Status { get; set; } = ItemStatus.Unavailable;
+        [JsonProperty("rate")]
+        public double SteamPriceRate
+        {
+            get => steamPriceRate / 100;
+            set => steamPriceRate = value;
+        }
+        private double steamPriceRate;
+
+        public bool IsHave => Tradable > 0;
+        public ItemStatus Status => Limit > 0 ? (Have >= Limit ? ItemStatus.Overstock : ItemStatus.Available) : ItemStatus.Unavailable;
     }
 }
