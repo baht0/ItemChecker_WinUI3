01b87c56fb1e89a6faba3c5c04fa924625bbf5d8
Rasim <bahtiarov.rasim@gmail.com>
2023-09-10 20:50:24 +0300

    remove check balance

--- ItemChecker/Models/StaticModels/Accounts/BuffAccount.cs
+++ ItemChecker/Models/StaticModels/Accounts/BuffAccount.cs
@@ -1,13 +1,11 @@
 ﻿using ItemChecker.Properties;
-using ItemChecker.Support;
-using System.Threading.Tasks;
 using static ItemChecker.Net.ServicesRequest;
 
 namespace ItemChecker.Models.StaticModels.Accounts
 {
     public class BuffAccount : Buff163
     {
-        public bool IsActive { get; private set; } = AppProperties.Services.BUFF;
+        public bool IsActive { get; private set; } = AppProperties.Account.BUFF;
         public double Balance { get; private set; }
         public double Commission => 0.975d;
 
@@ -17,13 +15,5 @@ namespace ItemChecker.Models.StaticModels.Accounts
                 SignInService();
         }
         private async void SignInService() => await Post.SignInAsync();
-        internal async Task GetBalanceAsync()
-        {
-            if (IsActive)
-            {
-                var blnInCny = await Get.BalanceAsync();
-                Balance = Currencies.ConverterToUsd(blnInCny, 23);
-            }
-        }
     }
 }
