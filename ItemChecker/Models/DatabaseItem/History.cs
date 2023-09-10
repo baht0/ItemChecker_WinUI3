365ea3f6728850d5247797c05a842736ad3b8cd0
Rasim <bahtiarov.rasim@gmail.com>
2023-09-10 20:35:40 +0300

    minnor fix

--- ItemChecker/Models/DatabaseItem/History.cs
+++ ItemChecker/Models/DatabaseItem/History.cs
@@ -4,70 +4,74 @@ using ItemChecker.Support;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace ItemChecker.Models.DatabaseItem
 {
     public partial class History : ObservableObject
     {
-        private Item _item;
-        [ObservableProperty]
-        bool _isBusy = true;
-        [ObservableProperty]
-        List<SaleHistory> _saleHistory;
-        [ObservableProperty]
-        Dictionary<string, object> _info = new();
+        private Item Item { get; set; }
 
         [ObservableProperty]
-        bool _isCount;
+        private bool isBusy = true;
         [ObservableProperty]
-        bool _isBuyOrder;
+        private List<SaleHistory> saleHistory;
+        [ObservableProperty]
+        private Dictionary<string, object> info = new();
 
-        public History(Item item, int id)
+        [ObservableProperty]
+        private bool isCount;
+        [ObservableProperty]
+        private bool isBuyOrder;
+
+        public History(Item item)
         {
-            _item = item;
+            Item = item;
             Info = new();
-            GetSaleHistory(id);
-            IsBusy = false;
+            IsBusy = true;
         }
-        private async void GetSaleHistory(int id)
+        public async Task GetSaleHistory(int id)
         {
+            var info = new Dictionary<string, object>();
             string currency = Currencies.Allow[0].Name;
             switch (id)
             {
                 case 0 or 1:
-                    await _item.UpdateSteamHistoryItem();
+                    await Item.UpdateSteamHistoryItem();
                     IsCount = true;
                     IsBuyOrder = false;
-                    SaleHistory = _item.Steam.History;
+                    SaleHistory = Item.Steam.History;
 
-                    Info.Add("LastSale:", SaleHistory.FirstOrDefault()?.Date);
+                    info.Add("LastSale:", SaleHistory.FirstOrDefault()?.Date);
 
                     var last30 = SaleHistory.Where(x => x.Date > DateTime.Today.AddDays(-30)).Select(x => x.Price).ToList();
                     var avg30 = last30.Any() ? Queryable.Average(last30.AsQueryable()) : 0;
-                    Info.Add($"Last 30 days Avg ({last30.Count}):", Math.Round(avg30, 2));
+                    info.Add($"Last 30 days Avg ({last30.Count}):", Math.Round(avg30, 2));
 
                     var last60 = SaleHistory.Where(x => x.Date > DateTime.Today.AddDays(-60)).Select(x => x.Price).ToList();
                     var avg60 = last60.Any() ? Queryable.Average(last60.AsQueryable()) : 0;
-                    Info.Add($"Last 60 days Avg ({last60.Count}):", Math.Round(avg60, 2));
+                    info.Add($"Last 60 days Avg ({last60.Count}):", Math.Round(avg60, 2));
 
                     currency = SteamAccount.Currency.Name;
                     break;
                 case 4 or 5:
-                    await _item.UpdateBuffHistoryItem();
+                    await Item.UpdateBuffHistoryItem();
                     IsCount = false;
                     IsBuyOrder = true;
-                    SaleHistory = _item.Buff.History;
+                    SaleHistory = Item.Buff.History;
 
-                    Info.Add("LastSale:", SaleHistory.FirstOrDefault()?.Date);
+                    info.Add("LastSale:", SaleHistory.FirstOrDefault()?.Date);
 
                     var avg = SaleHistory.Any() ? Queryable.Average(SaleHistory.Select(x => x.Price).AsQueryable()) : 0;
-                    Info.Add($"All sale Avg:", Math.Round(avg, 2));
+                    info.Add($"All sale Avg:", Math.Round(avg, 2));
 
                     avg = SaleHistory.Any() ? Queryable.Average(SaleHistory.Where(x => x.IsBuyOrder != true).Select(x => x.Price).AsQueryable()) : 0;
-                    Info.Add($"Without 'Supply' sale Avg:", Math.Round(avg, 2));
+                    info.Add($"Without 'Supply' sale Avg:", Math.Round(avg, 2));
                     break;
             };
-            Info.Add("Table price currency:", currency);
+            info.Add("Table price currency:", currency);
+            Info = info;
+            IsBusy = false;
         }
     }
 }
