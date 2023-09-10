1898dc9559544442498e6c5b20c03d89893d090c
Rasim <bahtiarov.rasim@gmail.com>
2023-09-10 20:58:13 +0300

    added messagebar

--- ItemChecker/Views/AccountPage.xaml.cs
+++ ItemChecker/Views/AccountPage.xaml.cs
@@ -1,34 +1,24 @@
-// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
-
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
+using ItemChecker.Views.Account;
 using Microsoft.UI.Xaml.Navigation;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace ItemChecker.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class AccountPage : Page
     {
+        readonly static InfoPage InfoPage = new();
+        readonly static InventoryPage InventoryPage = new();
         public AccountPage()
         {
             this.InitializeComponent();
+            Info.Content = InfoPage;
+            Inventory.Content = InventoryPage;
+        }
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            Info.Content = null;
+            Inventory.Content = null;
+            base.OnNavigatedFrom(e);
         }
     }
 }
